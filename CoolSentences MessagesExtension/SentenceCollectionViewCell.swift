056b19e12945c385efbed2bd539bd0edacabf490
Morteza <morinob93@gmail.com>
2018-09-18 19:11:41 -0400

    add a new controller for showing sentences

---
+++ CoolSentences MessagesExtension/SentenceCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SentenceCollectionViewCell.swift
+//  CoolSentences MessagesExtension
+//
+//  Created by Morteza Ahmadi on 2018-09-17.
+//  Copyright © 2018 Morteza Ahmadi. All rights reserved.
+//
+
+import Foundation
---
+++ CoolSentences MessagesExtension/SentencesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SentencesViewController.swift
+//  CoolSentences MessagesExtension
+//
+//  Created by Morteza Ahmadi on 2018-09-18.
+//  Copyright © 2018 Morteza Ahmadi. All rights reserved.
+//
+
+import Foundation
